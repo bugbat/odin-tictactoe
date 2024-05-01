97f7d764fda314c39f39bb0f7c457522e89b6e1c
bugbat <85291787+bugbat@users.noreply.github.com>
2024-04-30 22:36:34 -0400

    add start of gameboard logic

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // game board
-// player 1 & 2
+function createGameboard () {
+  let gameboard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+  const getGameboard = () => gameboard;
+  const updateGameboard = (marker, row, col) => {
+    gameboard[row][col] = marker
+  };
+
+  return {getGameboard, updateGameboard};
+}
+
+// player - 1 & 2 based on same object
 // win+tie checking
-// scoreboard
+// scoreboard - maybe just store in player object
+// game state
+
+// set up game
+board = createGameboard()
