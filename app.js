65ff37739cde018754429f60f33bc10db2f39951
bugbat <85291787+bugbat@users.noreply.github.com>
2024-05-01 08:08:48 -0400

    add some player logic

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
 // game board
-function createGameboard () {
+function createGameboard() {
   let gameboard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
 
   const getGameboard = () => gameboard;
   const updateGameboard = (marker, row, col) => {
-    gameboard[row][col] = marker
+    gameboard[row][col] = marker;
   };
 
   return {getGameboard, updateGameboard};
 }
 
 // player - 1 & 2 based on same object
+function createPlayer(name, marker) {
+  let score = 0;
+  const getName = () => name;
+  const getMarker = () => marker;
+
+  const getScore = () => score;
+  const addScore = () => ++score;
+  return {getName, getMarker, getScore, addScore}
+}
+
 // win+tie checking
 // scoreboard - maybe just store in player object
 // game state
 
 // set up game
-board = createGameboard()
+board = createGameboard();
+p = createPlayer('p1', 'x')
