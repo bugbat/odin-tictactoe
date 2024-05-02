a8cbd1e7f8a11bbe8732f2ebae2cfb04e65ea5c2
bugbat <85291787+bugbat@users.noreply.github.com>
2024-05-01 22:11:17 -0400

    add win checking logic

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // game board
 function createGameboard() {
-  let gameboard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  let gameboard = [['', '', ''], ['', '', ''], ['', '', '',]];
 
   const getGameboard = () => gameboard;
   const updateGameboard = (marker, row, col) => {
@@ -18,13 +18,46 @@ function createPlayer(name, marker) {
 
   const getScore = () => score;
   const addScore = () => ++score;
-  return {getName, getMarker, getScore, addScore}
+  return {getName, getMarker, getScore, addScore};
 }
 
 // win+tie checking
-// scoreboard - maybe just store in player object
+function checkForWinner(board) {
+  b = board.getGameboard();
+
+  // check diagonals for winner
+  if (b[0][0] === b[1][1] === b[2][2] && b[1][1] !== '') {
+    return b[0][0];
+  }
+  else if (b[0][2] === b[1][1] === b[2][0] && b[1][1] !== '') {
+    return b[0][2];
+  }
+
+  // check rows and cols for winner
+  for (i = 0; i < b.length; i++) {
+    if (b[i].every(mark => mark === b[i][0]) && b[i][0] !== '') {
+      return b[i][0];
+    }
+    else if (b[i][0] === b[i][1] === b[i][2] && b[i][0] !== '') {
+      return b[i][0];
+    }
+  }
+  return console.log('success')
+}
+
+// scoreboard - maybe just store in player object - done
 // game state
+function gameState(p1, p2, board) {
+  let gameActive = true;
+  let playerTurn = 'p1';
+}
+
+// prompt for move and place marker
+function getMove() {
+
+}
 
 // set up game
-board = createGameboard();
-p = createPlayer('p1', 'x')
+const myBoard = createGameboard();
+const p1 = createPlayer('p1', 'X');
+const p2 = createPlayer('p2', 'O');
