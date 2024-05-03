318b6839fd2190867aa7b5398d7dab63c94ab221
bugbat <85291787+bugbat@users.noreply.github.com>
2024-05-02 23:13:32 -0400

    add free space & tie checking

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 // game board
 function createGameboard() {
-  let gameboard = [['', '', ''], ['', '', ''], ['', '', '',]];
+  //let gameboard = [['', '', ''], ['', '', ''], ['', '', '']]; //empty
+  let gameboard = [['o', 'x', 'o'], ['x', 'o', 'x'], ['x', 'o', 'x']]; // tie
+  //let gameboard = [['x', 'x', 'x'], ['x', 'x', 'o'], ['o', 'x', 'x']]; // x
+  //let gameboard = [['o', 'o', 'o'], ['x', 'x', 'o'], ['o', 'x', 'x']]; // o
 
   const getGameboard = () => gameboard;
   const updateGameboard = (marker, row, col) => {
     gameboard[row][col] = marker;
   };
 
-  return {getGameboard, updateGameboard};
+  return { getGameboard, updateGameboard };
 }
 
 // player - 1 & 2 based on same object
@@ -18,12 +21,12 @@ function createPlayer(name, marker) {
 
   const getScore = () => score;
   const addScore = () => ++score;
-  return {getName, getMarker, getScore, addScore};
+  return { getName, getMarker, getScore, addScore };
 }
 
 // win+tie checking
 function checkForWinner(board) {
-  b = board.getGameboard();
+  const b = board.getGameboard();
 
   // check diagonals for winner
   if (b[0][0] === b[1][1] === b[2][2] && b[1][1] !== '') {
@@ -42,10 +45,37 @@ function checkForWinner(board) {
       return b[i][0];
     }
   }
-  return console.log('success')
+  // if no winner, check for tie
+  if (checkFreeSpace(board) === false) {
+    return 'tie';
+  }
+  // if no win and no tie, go to next round
+  return false;
 }
 
-// scoreboard - maybe just store in player object - done
+// check if board full
+function checkFreeSpace(board, coord = null) {
+  const b = board.getGameboard();
+  if (coord === null) {
+    let fullBoard = []
+    for (i = 0; i < b.length; i++) {
+      fullBoard.push(...b[i]);
+      }
+    if (!fullBoard.includes('')) {
+      return false;
+    }
+  }
+  else if (coord !== null) {
+    const yx = coord.split('');
+    const y = Number(yx[0]);
+    const x = Number(yx[1]);
+    if (b[y][x] !== '') {
+      return false;
+    }
+  }
+  return true;
+}
+
 // game state
 function gameState(p1, p2, board) {
   let gameActive = true;
