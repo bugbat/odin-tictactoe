5b3c6b6938a4435eb6602155e7ffd2b23399898b
bugbat <85291787+bugbat@users.noreply.github.com>
2024-05-06 22:31:27 -0400

    add stylesheet, start of dom logic, game turn flow

--- app.js
+++ app.js
@@ -26,23 +26,21 @@ function createPlayer(name, marker) {
 
 // win+tie checking
 function checkForWinner(board) {
-  const b = board.getGameboard();
-
   // check diagonals for winner
-  if (b[0][0] === b[1][1] === b[2][2] && b[1][1] !== '') {
-    return b[0][0];
+  if (board[0][0] === board[1][1] === board[2][2] && board[1][1] !== '') {
+    return board[0][0];
   }
-  else if (b[0][2] === b[1][1] === b[2][0] && b[1][1] !== '') {
-    return b[0][2];
+  else if (board[0][2] === board[1][1] === board[2][0] && board[1][1] !== '') {
+    return board[0][2];
   }
 
   // check rows and cols for winner
-  for (i = 0; i < b.length; i++) {
-    if (b[i].every(mark => mark === b[i][0]) && b[i][0] !== '') {
-      return b[i][0];
+  for (i = 0; i < board.length; i++) {
+    if (board[i].every(mark => mark === board[i][0]) && board[i][0] !== '') {
+      return board[i][0];
     }
-    else if (b[i][0] === b[i][1] === b[i][2] && b[i][0] !== '') {
-      return b[i][0];
+    else if (board[i][0] === board[i][1] === board[i][2] && board[i][0] !== '') {
+      return board[i][0];
     }
   }
   // if no winner, check for tie
@@ -55,19 +53,20 @@ function checkForWinner(board) {
 
 // check if board full
 function checkFreeSpace(board, coord = null) {
-  const b = board.getGameboard();
+  // if given no coordinate, checks it the board is full
   if (coord === null) {
     let fullBoard = []
-    for (i = 0; i < b.length; i++) {
-      fullBoard.push(...b[i]);
+    for (i = 0; i < board.length; i++) {
+      fullBoard.push(...board[i]);
       }
     if (!fullBoard.includes('')) {
       return false;
     }
   }
+
+  // if given coordinate, checks that specific space
   else if (coord !== null) {
-    const coord_dict = convertInput(coord)
-    if (b[coord_dict.row][coord_dict.col] !== '') {
+    if (board[coord_dict.row][coord_dict.col] !== '') {
       return false;
     }
   }
@@ -91,14 +90,53 @@ function createGame() {
   const p2 = createPlayer('p2', 'O');
 
   let active = true;
-  let playerTurn = 'p1';
-  return { board, p1, p2, active, playerTurn }
+  let currentPlayer = p1;
+  const getCurrentPlayer = () => currentPlayer;
+
+
+  const swapCurrentPlayer = () => {
+    if (currentPlayer === p1) {
+      currentPlayer = p2;
+    }
+    else {
+      currentPlayer = p1;
+    }
+  }
+
+  return { board, p1, p2, active, getCurrentPlayer, swapCurrentPlayer }
 }
 
-// prompt for move and place marker
-function getMove() {
+// 1 'turn' of the game
+function gameRound(game, input) {
+  board = game.getGameboard();
+  move = convertInput(input);
+  if (checkFreeSpace(board, move)) {
 
+  }
 }
 
-const game = createGame()
+// set up dom variables
 
+// updates player info panel with game elements
+function updatePlayerInfo(game) {
+  const div = document.querySelector("#playerinfo");
+  while(div.firstChild) { 
+    div.removeChild(div.firstChild); 
+  }
+  
+  const p1 = document.createElement("p");
+  p1.textContent = "Player 1: " + game.p1.getName();
+  const p2 = document.createElement("p");
+  p2.textContent = "Player 2: " + game.p2.getName();
+
+  const turn = document.createElement("p");
+  turn.textContent = game.getCurrentPlayer().getName();
+
+
+  div.append(p1, p2, turn);
+}
+
+// initiate game
+const myGame = createGame()
+
+updatePlayerInfo(myGame)
